79a951afae2df437d8f668435ca349c629893b1b
Michael Tang <92874177+mtang08@users.noreply.github.com>
2026-04-06 23:12:27 -0400

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [0] * (2 * n)
+        for i, num in enumerate(nums):
+            res[i] = num
+            res[i + n] = num
+
+        return res
+
+'''
+[1,4,1,2]
+       0 1 2 3 4 5 6 7
+res = [1,4,1,2,1,4,1,2]
+'''
