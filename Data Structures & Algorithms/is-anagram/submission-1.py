71e3c74b636e7cab439406f1ddc79ed6d0242f7b
Michael Tang <92874177+mtang08@users.noreply.github.com>
2026-04-06 23:25:18 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+            
+        # count the char frequencies of string s
+        count = {}
+        for c in s:
+            if c not in count:
+                count[c] = 0
+            count[c] += 1
+        
+        # iterate over chars in t, decrement on match
+        for c in t:
+            if c not in count:
+                return False
+            count[c] -= 1
+        
+        for c, freq in count.items():
+            if freq != 0:
+                return False
+
+        return True
