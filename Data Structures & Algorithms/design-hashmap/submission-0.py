fb364175a2b3b9fdcee9b6c80861782e8b1ed8c8
Michael Tang <92874177+mtang08@users.noreply.github.com>
2026-04-10 23:29:57 -0400

    Add: design-hashmap - submission-0

---
+++ Data Structures & Algorithms/design-hashmap/submission-0.py
@@ -0,0 +1,34 @@
+class MyHashMap:
+    def __init__(self):
+        self.MOD = 1009
+        # list of buckets of key-value tuples
+        self.store = [[] for _ in range(self.MOD)]
+
+    def put(self, key: int, value: int) -> None:
+        bucket = self.store[key % self.MOD]
+        tup = (key, value)
+        for i, (k, _) in enumerate(bucket):
+            if k == key:
+                bucket[i] = tup
+                return
+        bucket.append(tup)
+
+    def get(self, key: int) -> int:
+        bucket = self.store[key % self.MOD]
+        for (k, v) in bucket:
+            if k == key:
+                return v
+        return -1
+
+    def remove(self, key: int) -> None:
+        bucket = self.store[key % self.MOD]
+        for i, (k, v) in enumerate(bucket):
+            if k == key:
+                bucket.pop(i)
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
